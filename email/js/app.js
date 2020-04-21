768d251576c3f8d16fb0915494dd53789b299ba1
Ramiro Cerdá <rami992009@gmail.com>
2020-04-21 10:33:33 -0300

    validacion de campos

--- email/js/app.js
+++ email/js/app.js
@@ -8,8 +8,46 @@ eventListeners();
 
 function eventListeners() {
   document.addEventListener("DOMContentLoaded", inicioApp);
+  email.addEventListener("blur", validarCampo);
+  asunto.addEventListener("blur", validarCampo);
+  mensaje.addEventListener("blur", validarCampo);
 }
 
 function inicioApp() {
   btnEnviar.disabled = true;
+}
+
+function validarCampo() {
+  validarLongitud(this);
+
+  if (this.type === "email") validarEmail(this);
+
+  const errores = document.querySelectorAll(".error");
+  if (
+    errores.length === 0 &&
+    email.value !== "" &&
+    asunto.value !== "" &&
+    mensaje.value !== ""
+  )
+    btnEnviar.disabled = false;
+}
+function validarLongitud(campo) {
+  if (campo.value.length > 0) {
+    campo.style.borderBottomColor = "green";
+    campo.classList.remove("error");
+  } else {
+    campo.style.borderBottomColor = "red";
+    campo.classList.add("error");
+  }
+}
+
+function validarEmail(email) {
+  const contenidoEmail = email.value;
+  if (contenidoEmail.indexOf("@") !== -1) {
+    email.style.borderBottomColor = "green";
+    email.classList.remove("error");
+  } else {
+    email.style.borderBottomColor = "red";
+    email.classList.add("error");
+  }
 }
