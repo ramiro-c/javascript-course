3a7fa02a8c68a4864cb045b15b69d72e25f68a57
Ramiro Cerdá <rami992009@gmail.com>
2020-04-25 22:14:42 -0300

    realizar calculo de cotizacion del seguro

--- cotizar-seguro/js/app.js
+++ cotizar-seguro/js/app.js
@@ -4,6 +4,35 @@ class Seguro {
     this.anio = anio;
     this.tipo = tipo;
   }
+  cotizar() {
+    // 1 americano 1.15
+    // 2 asiatico 1.05
+    // 3 europeo 1.35
+    let cantidad = 0;
+    const precioBase = 2000;
+
+    switch (this.marca) {
+      case "1":
+        cantidad = base * 1.15;
+        break;
+      case "2":
+        cantidad = base * 1.05;
+        break;
+      case "3":
+        cantidad = base * 1.35;
+        break;
+      default:
+        break;
+    }
+    // leer el anio
+    const diferencia = new Date().getFullYear() - this.anio;
+    // cada anio de diferencia hay que reducir un 3% el valor
+    cantidad -= diferencia * cantidad * 0.03;
+    // basico se agrega un 30% mas
+    // completo se agrega un 50% mas
+    if (this.tipo === "basico") return cantidad * 1.3;
+    return cantidad * 1.5;
+  }
 }
 
 // todo lo que se muestra en pantalla
@@ -47,6 +76,8 @@ formulario.addEventListener("submit", function (e) {
       "error"
     );
   } else {
+    const seguro = new Seguro(marcaSeleccionada, anioSeleccionado, tipo);
+    const cantidad = seguro.cotizar();
   }
 });
 
